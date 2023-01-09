b533ab6fac543de9cdc7e38a9d69d429537f3edd
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2023-01-09 23:10:10 +0530

    Time: 0 ms (100.00%), Space: 6 MB (5.13%) - LeetHub

---
+++ 319-bulb-switcher/319-bulb-switcher.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int bulbSwitch(int n) {
+        return sqrt(n); 
+    }
+}; 
+/* 0 - off 
+1 - on
+
+R0 0 0 0 0 0
+R1 1 1 1 1 1
+R2 1 0 1 0 1
+R3 1 0 0 0 1 
+R4 1 0 0 1 1
+R5 1 0 0 1 0 
+*/
