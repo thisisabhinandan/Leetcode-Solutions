b032548ff3b58ef44b632b40b9121e831a2ef480
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-04-26 09:16:40 +0530

    Time: 59 ms (14.64%), Space: 51.9 MB (10.38%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,16 +1,20 @@
 class Solution {
 public:
-    int solve(vector<int>&coins, int amount, int n, vector<vector<int>>& dp)
+    int solve(vector<int>&coins, int amount, int i, vector<vector<int>>& dp)
     {
-        if(amount==0) return 0;
-        if(n >=coins.size() || amount < 0) return INT_MAX-1;
-        if(dp[n][amount]!=-1) return dp[n][amount]; 
-        return dp[n][amount]=min(solve(coins,amount,n+1,dp),1+solve(coins,amount-coins[n],n,dp));
-        
+       if(amount == 0) return 0; 
+        if(i>=coins.size() || amount<0) return 1e9; 
+        if(dp[i][amount]!=-1) return dp[i][amount]; 
+        int nt=0;
+        nt=solve(coins,amount,i+1,dp);
+        int t=0;
+        t=1+solve(coins,amount-coins[i],i,dp);
+        return dp[i][amount]=min(t,nt);
     }
     int coinChange(vector<int>& coins, int amount) {
         vector<vector<int>> dp (coins.size()+1,vector<int>(amount+1,-1));
         int ans= solve(coins,amount,0,dp);
-        return (ans < INT_MAX-1)?ans:-1;
+        if(ans==1e9) return -1; 
+        else return ans;
     }
 };
