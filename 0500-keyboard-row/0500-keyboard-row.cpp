137d66a727fcb516070cd9ef8512204e5d417e99
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-05-01 21:05:37 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (51.95%) - LeetHub

--- 0500-keyboard-row/0500-keyboard-row.cpp
+++ 0500-keyboard-row/0500-keyboard-row.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     vector<string> findWords(vector<string>& words) {
-        string s1="QWERTYUIOPqwertyuiop";
-        string s2="ASDFGHJKLasdfghjkl";
-        string s3="ZXCVBNMzxcvbnm";
+        string s1="qwertyuiop";
+        string s2="asdfghjkl";
+        string s3="zxcvbnm";
         
         unordered_map<char,int>mp1,mp2,mp3; vector<string> ans;
         
@@ -14,14 +14,12 @@ public:
         for(int i=0;i<words.size();i++)
         {
             string s=words[i]; 
-            
             int c1=0; int c2=0; int c3=0;
-        
             for(int j=0;j<s.length();j++)
             {
-                if(mp1.find(s[j])!=mp1.end()) c1++;
-                if(mp2.find(s[j])!=mp2.end()) c2++;
-                if(mp3.find(s[j])!=mp3.end()) c3++;
+                if(mp1.find(tolower(s[j]))!=mp1.end()) c1++;
+                if(mp2.find(tolower(s[j]))!=mp2.end()) c2++;
+                if(mp3.find(tolower(s[j]))!=mp3.end()) c3++;
             }
             
             if(c1==s.length() || c2==s.length() || c3==s.length()) ans.push_back(s);
