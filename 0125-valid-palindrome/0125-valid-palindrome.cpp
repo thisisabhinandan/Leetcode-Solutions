f7530094f5edcb764e126dbb11e8773fac8696b1
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-05-02 16:52:08 +0530

    Time: 6 ms (46.11%), Space: 9.1 MB (30.26%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool isPalindrome(string str) {
+        string s="";
+        for(int i=0;i<str.length();i++)
+        {
+            if(isalnum(str[i])) s+=tolower(str[i]);
+        }
+        int l=0; int r=s.length()-1;
+        while(l<r)
+        {
+            if(s[l]!=s[r]) return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+};
