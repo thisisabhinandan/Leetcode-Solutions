df5270e9a432a5041ab22ef4df4d0b3d7610f939
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-05-02 17:54:34 +0530

    Time: 12 ms (58.76%), Space: 11 MB (39.63%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     vector<int> findAnagrams(string s, string p) {
-        vector<int> ans; 
-        unordered_map<char,int> mp;
-        for(int i=0;i<p.length();i++) mp[p[i]]++;
-        int j=0;
-        for(int i=0;i<s.length();i++)
-        {   bool b=true;
-            mp[s[i]]--;
-            if(i-j+1==p.length())
-            {
-                for(int k=0;k<p.length();k++)
-                {
-                    if(mp[p[k]]!=0)
-                    {
-                        b=false; break; 
+        unordered_map<char,int>mpp;
+        vector<int>ans;
+        for(auto it : p){
+            mpp[it]--;
+        }
+        for(int i=0,j=0;j<s.size();j++){
+            mpp[s[j]]++;
+            if(j-i+1==p.size()){
+                bool flag=1;
+                for(auto it : mpp){
+                    if(it.second!=0){
+                        flag=0;break;
                     }
                 }
-                if(b==true) ans.push_back(j);
-                mp[s[j]]++;
-                j++;
+                if(flag){
+                    ans.push_back(i);
+                }
+                mpp[s[i]]--;
+                i++;
             }
         }
         return ans;
