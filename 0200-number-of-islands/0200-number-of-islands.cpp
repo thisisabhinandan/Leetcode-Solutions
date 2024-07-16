062e50f6b1ea12dfef993d936d1fd8480eac8dc1
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-07-16 22:36:10 +0530

    Time: 37 ms (21.07%), Space: 24.1 MB (13.68%) - LeetHub

---
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        int cnt = 0;
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (!visited[i][j] && grid[i][j] == '1') {
+                    cnt++;
+                    bfs(grid, i, j, visited);
+                }
+            }
+        }
+        return cnt;
+    }
+
+    void bfs(vector<vector<char>>& grid, int i, int j, vector<vector<int>>& visited) {
+        int dr[] = {1, 0, -1, 0};
+        int dc[] = {0, 1, 0, -1};
+        queue<pair<int, int>> q;
+        visited[i][j] = true;
+        q.push({i, j});
+
+        while (!q.empty()) {
+            int r = q.front().first;
+            int c = q.front().second;
+            q.pop();
+
+            for (int k = 0; k < 4; k++) {
+                int nr = r + dr[k];
+                int nc = c + dc[k];
+
+                if (nr >= 0 && nr < grid.size() && nc >= 0 && nc < grid[0].size() && !visited[nr][nc] && grid[nr][nc] == '1') {
+                    visited[nr][nc] = true;
+                    q.push({nr, nc});
+                }
+            }
+        }
+    }
+};
