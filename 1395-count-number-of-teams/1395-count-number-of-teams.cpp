553ed2d426d83be366c0406c8d6e9d56ac1a9c34
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-07-22 12:46:15 +0530

    Time: 24 ms (87.31%), Space: 11.6 MB (46.68%) - LeetHub

--- 1395-count-number-of-teams/1395-count-number-of-teams.cpp
+++ 1395-count-number-of-teams/1395-count-number-of-teams.cpp
@@ -1,37 +1,31 @@
-#include <vector>
-#include <iostream>
-using namespace std;
-
 class Solution {
 public:
     int numTeams(vector<int>& rating) {
-        int n = rating.size();
-        int totalTeams = 0;
-
-        for (int i = 0; i < n; ++i) {
-            int lessLeft = 0, greaterRight = 0;
-            int lessRight = 0, greaterLeft = 0;
-
-            for (int j = 0; j < i; ++j) {
-                if (rating[j] < rating[i]) {
-                    lessLeft++;
-                } else if (rating[j] > rating[i]) {
-                    greaterLeft++;
+       int n=rating.size();
+        int teamcount=0;
+        for(int i=0;i<n;i++)
+        {
+            int greaterleft=0; int greaterright=0;
+            int lesserleft=0; int lesserright=0; 
+            for(int j=i+1;j<n;j++)
+            {
+                if(rating[j]>rating[i])
+                {
+                    greaterright++;
                 }
+                else lesserright++;
             }
-            for (int k = i + 1; k < n; ++k) {
-                if (rating[k] > rating[i]) {
-                    greaterRight++;
-                } else if (rating[k] < rating[i]) {
-                    lessRight++;
+            for(int k=0;k<i;k++)
+            {
+                if(rating[k]>rating[i])
+                {
+                    greaterleft++;
                 }
+                else lesserleft++;
             }
-
-            totalTeams += lessLeft * greaterRight; // case 1: rating[i] is the middle element
-            totalTeams += greaterLeft * lessRight; // case 2: rating[i] is the middle element
+            teamcount+=(lesserright*greaterleft)+(lesserleft*greaterright);
         }
-
-        return totalTeams;
+        return teamcount;
     }
 };
 
