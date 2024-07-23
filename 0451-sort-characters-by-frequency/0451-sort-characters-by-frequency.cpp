4c104adc084a9705c7b9354e0e312aabb094d5e2
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-07-23 22:08:05 +0530

    Time: 16 ms (10.67%), Space: 17 MB (5.05%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.cpp
@@ -1,22 +1,27 @@
-
 class Solution {
 public:
     string frequencySort(string s) {
-        string ans=""; 
-        unordered_map<char,int> mp; 
-        for(int i=0;i<s.length();i++)
-        {
-            mp[s[i]]++; 
+        string ans;
+        unordered_map<char, int> mp;
+
+        // Count the frequency of each character
+        for (int i = 0; i < s.length(); i++) {
+            mp[s[i]]++;
         }
-        vector<pair<char,int>> freq(mp.begin(),mp.end());
-        sort(freq.begin(),freq.end(), [](pair<char,int>& a, pair<char,int>& b)
-             {
-                 return a.second>b.second; 
-             });
-        for(int i=0;i<freq.size();i++)
-        {
-            ans+=string(freq[i].second,freq[i].first);
+
+        // Create a bucket array to store characters with same frequency
+        vector<vector<char>> bucket(s.length() + 1);
+        for (auto& p : mp) {
+            bucket[p.second].push_back(p.first);
         }
+
+        // Construct the result string
+        for (int i = s.length(); i > 0; i--) {
+            for (char c : bucket[i]) {
+                ans += string(i, c);
+            }
+        }
+
         return ans;
     }
 };
