0b6223f4d32f7d3945ac1ce434954caa3afb3d04
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-07-24 09:20:42 +0530

    Time: 17 ms (34.05%), Space: 20.4 MB (23.71%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -1,21 +1,21 @@
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
-        int n=ratings.size(); 
+    int candy(vector<int>& rating) {
+        int n=rating.size(); 
         vector<int> ans(n,1);
         int sum=0;
         for(int i=1;i<n;i++)
         {
-            if(ratings[i]>ratings[i-1])
+            if(rating[i]>rating[i-1])
             {
                 ans[i]=ans[i-1]+1; 
             }
         }
         for(int i=n-1;i>0;i--)
         {
-            if(ratings[i]<ratings[i-1])
+            if(rating[i]<rating[i-1])
             {
-                ans[i-1]=max(ans[i]+1,ans[i-1]);
+                ans[i-1]=max(ans[i-1],ans[i]+1);
             }
         }
         for(int i=0;i<n;i++)
