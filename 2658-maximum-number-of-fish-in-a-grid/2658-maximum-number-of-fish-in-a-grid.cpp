252bb01b460ccd9a82fcb28462c3ab1a1ba61fc6
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-07-27 16:14:16 +0530

    Time: 143 ms (6.71%), Space: 153.5 MB (5.10%) - LeetHub

--- 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
+++ 2658-maximum-number-of-fish-in-a-grid/2658-maximum-number-of-fish-in-a-grid.cpp
@@ -10,7 +10,7 @@ public:
         int ans = 0;
         for (int i = 0; i < grid.size(); i++) {
             for (int j = 0; j < grid[0].size(); j++) {
-                if (grid[i][j] > 0) { // Start BFS only from water cells with fish
+                if (grid[i][j] != 0) {
                     ans = max(ans, bfs(grid, i, j));
                 }
             }
@@ -18,41 +18,38 @@ public:
         return ans;
     }
 
-private:
     int bfs(vector<vector<int>>& grid, int i, int j) {
         int n = grid.size();
         int m = grid[0].size();
         queue<pair<int, int>> q;
         int sum = 0;
-        vector<vector<bool>> vis(n, vector<bool>(m, false));
-        
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        vis[i][j] = 1;
         q.push({i, j});
-        vis[i][j] = true;
+        sum += grid[i][j]; // Add the fish count from the starting cell
         
         int dr[] = {0, -1, 0, 1};
         int dc[] = {1, 0, -1, 0};
-
+        
         while (!q.empty()) {
             int r = q.front().first;
             int c = q.front().second;
             q.pop();
-
-            sum += grid[r][c];
-
+            
             for (int k = 0; k < 4; k++) {
                 int nr = r + dr[k];
                 int nc = c + dc[k];
-                if (isValid(nr, nc, n, m) && grid[nr][nc] > 0 && !vis[nr][nc]) {
+                if (isvalid(nr, nc, n, m) && vis[nr][nc] == 0 && grid[nr][nc] != 0) {
+                    sum += grid[nr][nc];
+                    vis[nr][nc] = 1;
                     q.push({nr, nc});
-                    vis[nr][nc] = true;
                 }
             }
         }
-
         return sum;
     }
 
-    bool isValid(int nr, int nc, int n, int m) {
+    bool isvalid(int nr, int nc, int n, int m) {
         return nr >= 0 && nc >= 0 && nr < n && nc < m;
     }
 };
