38d1150a05b626614326f21f51cffa1a2cdf4b26
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-07-29 18:09:14 +0530

    Time: 20 ms (12.59%), Space: 51.8 MB (25.89%) - LeetHub

--- 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
+++ 2706-buy-two-chocolates/2706-buy-two-chocolates.cpp
@@ -1,8 +1,25 @@
 class Solution {
 public:
     int buyChoco(vector<int>& prices, int money) {
-        sort(prices.begin(),prices.end()); 
-        if((prices[0]+prices[1])<=money) return abs(prices[0]+prices[1]-money);
-        else return money;
+        int mini=*min_element(prices.begin(),prices.end());
+        int smini=1e9; int cnt=0;
+        for(int i=0;i<prices.size();i++)
+        {
+            if(prices[i]==mini)
+            {
+                cnt++;
+                if(cnt>1)
+                {
+                 if(2*mini<=money)
+                {
+                    return money-2*mini;
+                }
+                else return money;   
+                }
+            }
+         else smini=min(smini,prices[i]);
+        }
+        if(mini+smini<=money) return money-mini-smini;
+        else return money; 
     }
 };
