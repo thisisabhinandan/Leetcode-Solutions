c6718194fb358ecacfc699a62d72357ae17bf387
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-07-31 09:58:05 +0530

    Time: 24 ms (55.31%), Space: 31.2 MB (56.38%) - LeetHub

--- 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
+++ 0930-binary-subarrays-with-sum/0930-binary-subarrays-with-sum.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     int atmost(vector<int>& nums, int goal)
-    {
-         
+    {    
         int ans=0; int j=0; int sum=0; int n=nums.size();
         for(int i=0;i<nums.size();i++)
         {
             sum+=nums[i];
             
-            while(sum>goal && i>=j)
+            while(sum>goal&&i>=j)
             {
                 sum-=nums[j];
                 j++;
