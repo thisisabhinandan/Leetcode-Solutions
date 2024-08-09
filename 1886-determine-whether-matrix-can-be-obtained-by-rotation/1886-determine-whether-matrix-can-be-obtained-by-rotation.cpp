f2367ba4a4c8a4f8bce3ba4f524b920b71c777c8
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-08-09 20:19:00 +0530

    Time: 0 ms (100.00%), Space: 14.2 MB (5.83%) - LeetHub

--- 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 1886-determine-whether-matrix-can-be-obtained-by-rotation/1886-determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,33 +1,35 @@
 class Solution {
 public:
     bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target) {
-        vector<vector<int>>temp1 = mat; int n=mat.size();
+        int n=mat.size(); int m=mat[0].size();
+        vector<vector<int>> temp1=mat;
         reverse(temp1.begin(),temp1.end());
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i+1;j<m;j++)
             {
-                swap(temp1[i][j],temp1[j][i]);
+               swap(temp1[i][j],temp1[j][i]);
             }
         }
-        vector<vector<int>>temp2=temp1;
-         reverse(temp2.begin(),temp2.end());
+        vector<vector<int>> temp2=temp1;
+        reverse(temp2.begin(),temp2.end());
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i+1;j<m;j++)
             {
                 swap(temp2[i][j],temp2[j][i]);
             }
         }
-        vector<vector<int>>temp3=temp2;
-         reverse(temp3.begin(),temp3.end());
+        vector<vector<int>> temp3=temp2;
+        reverse(temp3.begin(),temp3.end());
         for(int i=0;i<n;i++)
         {
-            for(int j=i+1;j<n;j++)
+            for(int j=i+1;j<m;j++)
             {
-                swap(temp3[i][j],temp3[j][i]);
+                 swap(temp3[i][j],temp3[j][i]);
             }
         }
-        return mat==target||temp1==target||temp2==target||temp3==target; 
+        if(temp1==target||temp2==target||temp3==target||mat==target) return true;
+        else return false;
     }
 };
