4be85506f49877e37f8ad069c24f15b25e1317b1
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-08-17 22:37:13 +0530

    Time: 223 ms (100.00%), Space: 183.1 MB (100.00%) - LeetHub

--- 3255-find-the-power-of-k-size-subarrays-ii/3255-find-the-power-of-k-size-subarrays-ii.cpp
+++ 3255-find-the-power-of-k-size-subarrays-ii/3255-find-the-power-of-k-size-subarrays-ii.cpp
@@ -3,20 +3,19 @@ public:
     vector<int> resultsArray(vector<int>& nums, int k) {
         int n = nums.size();
         vector<int> ans;
-        int consecutive = 1;
+        int idx = -1;  // Track where consecutive sequence breaks
 
         for (int i = 0; i < n; i++) {
-            if (i > 0 && nums[i] == nums[i-1] + 1) {
-                consecutive++;
-            } else {
-                consecutive = 1;
+            if (i > 0 && nums[i] != nums[i-1] + 1) {
+                idx = i;
             }
 
             if (i >= k - 1) {
-                if (consecutive >= k) {
-                    ans.push_back(nums[i]);
-                } else {
+                int j = i - k + 1;
+                if (j < idx) {
                     ans.push_back(-1);
+                } else {
+                    ans.push_back(nums[i]);
                 }
             }
         }
