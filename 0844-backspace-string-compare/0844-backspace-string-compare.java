fdee359c0f96b786d8effe5be2117070e737cf4b
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-08-21 19:37:50 +0530

    Time: 2 ms (57.73%), Space: 42.1 MB (9.02%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.java
+++ 0844-backspace-string-compare/0844-backspace-string-compare.java
@@ -1,40 +1,21 @@
-import java.util.Stack;
-
 class Solution {
     public boolean backspaceCompare(String s, String t) {
-        Stack<Character> st1 = new Stack<>();
-        Stack<Character> st2 = new Stack<>();
-
-        // Process string s
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) == '#') {
-                if (!st1.isEmpty()) {
-                    st1.pop();
-                }
-            } else {
-                st1.push(s.charAt(i));
-            }
-        }
-
-        // Process string t
-        for (int i = 0; i < t.length(); i++) {
-            if (t.charAt(i) == '#') {
-                if (!st2.isEmpty()) {
-                    st2.pop();
-                }
-            } else {
-                st2.push(t.charAt(i));
-            }
-        }
-
-        // Compare the two stacks
-        while (!st1.isEmpty() && !st2.isEmpty()) {
-            if (st1.pop() != st2.pop()) {
-                return false;
-            }
-        }
-
-        // If both stacks are empty, strings are equivalent
-        return st1.isEmpty() && st2.isEmpty();
+         Stack<Character> st1=new Stack<>();
+         Stack<Character> st2=new Stack<>();
+         for(int i=0;i<s.length();i++)
+         {
+             if(!st1.isEmpty()&&s.charAt(i)=='#') st1.pop();
+             else if(s.charAt(i)!='#') st1.push(s.charAt(i));
+         }
+         for(int i=0;i<t.length();i++)
+         {
+             if(!st2.isEmpty()&&t.charAt(i)=='#') st2.pop();
+             else if(t.charAt(i)!='#') st2.push(t.charAt(i));
+         }
+         while(!st1.isEmpty()&&!st2.isEmpty())
+         {
+             if(st1.pop()!=st2.pop()) return false;
+         }
+        return st1.isEmpty()&&st2.isEmpty();
     }
 }
