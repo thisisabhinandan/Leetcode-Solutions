53bb26d5a09ade7e8e04611e1e31d25ab798a891
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-09-30 16:25:46 +0530

    Time: 9 ms (67.75%), Space: 60.3 MB (81.40%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.java
@@ -1,11 +1,12 @@
 class Solution {
     public int minCost(String colors, int[] neededTime) {
-        int ans = 0;
-        for (int i = 1; i < colors.length(); i++) {
-            if (colors.charAt(i) == colors.charAt(i - 1)) {
-                ans += Math.min(neededTime[i], neededTime[i - 1]);
-                // Keep the balloon with the larger needed time for the next comparison
-                neededTime[i] = Math.max(neededTime[i], neededTime[i - 1]);
+        int ans=0;
+        for(int i=1;i<colors.length();i++)
+        {
+            if(colors.charAt(i)==colors.charAt(i-1))
+            {
+                ans+=Math.min(neededTime[i-1],neededTime[i]);
+                neededTime[i]=Math.max(neededTime[i],neededTime[i-1]); //ele w min time is removed and element with highest time is kept
             }
         }
         return ans;
