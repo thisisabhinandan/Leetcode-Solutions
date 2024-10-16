fc114338bff2a7a216d4149b8cf965c21ef57d1b
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-10-16 10:32:01 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (76.87%) - LeetHub

---
+++ 1405-longest-happy-string/1405-longest-happy-string.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String longestDiverseString(int a, int b, int c) {
+        StringBuilder sb = new StringBuilder();
+        int cnta = 0, cntb = 0, cntc = 0;
+
+        while (a > 0 || b > 0 || c > 0) {
+            if ((a >= b && a >= c && cnta < 2) || (cntb == 2 && a > 0) || (cntc == 2 && a > 0)) {
+                sb.append('a');
+                a--;
+                cnta++;
+                cntb = 0;
+                cntc = 0;
+            } else if ((b >= a && b >= c && cntb < 2) || (cnta == 2 && b > 0) || (cntc == 2 && b > 0)) {
+                sb.append('b');
+                b--;
+                cntb++;
+                cnta = 0;
+                cntc = 0;
+            } else if ((c >= a && c >= b && cntc < 2) || (cnta == 2 && c > 0) || (cntb == 2 && c > 0)) {
+                sb.append('c');
+                c--;
+                cntc++;
+                cnta = 0;
+                cntb = 0;
+            } else {
+                break;
+            }
+        }
+
+        return sb.toString();
+    }
+}
