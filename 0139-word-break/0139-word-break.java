d81ede980f481745aa2db20f908699542070dfa6
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-10-19 12:00:51 +0530

    Time: 8 ms (42.42%), Space: 44.7 MB (22.76%) - LeetHub

--- 0139-word-break/0139-word-break.java
+++ 0139-word-break/0139-word-break.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean wordBreak(String s, List<String> wordDict) {
+        boolean [] dp=new boolean[s.length()+1]; 
         Set<String> st=new HashSet<>(wordDict);
-        boolean [] dp=new boolean[s.length()+1];
-        dp[0]=true;
+        dp[0]=true; //can always make the first substring
         for(int i=1;i<=s.length();i++)
         {
             for(int j=0;j<i;j++)
             {
+                //if we can make smaller then true;
                 if(dp[j] && st.contains(s.substring(j,i)))
                 {
                     dp[i]=true; 
