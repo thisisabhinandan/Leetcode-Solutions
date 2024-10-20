7050f6ec99ba00420ba83bdfa15321ea5545965e
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-10-20 11:01:31 +0530

    Time: 24 ms (100.00%), Space: 56 MB (100.00%) - LeetHub

--- 3324-find-the-sequence-of-strings-appeared-on-the-screen/3324-find-the-sequence-of-strings-appeared-on-the-screen.java
+++ 3324-find-the-sequence-of-strings-appeared-on-the-screen/3324-find-the-sequence-of-strings-appeared-on-the-screen.java
@@ -1,23 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
 class Solution {
     public List<String> stringSequence(String target) {
-        List<String> list=new ArrayList<>(); 
-        StringBuilder sb=new StringBuilder(); 
-        for(char ch:target.toCharArray())
-        {
-            sb.append('a');
-            list.add(sb.toString());
-            while(sb.charAt(sb.length()-1)!=ch)
-            {
-                char lastdigit=sb.charAt(sb.length()-1);
-                if(lastdigit=='z')
-                {
-                    lastdigit='a';
+        List<String> list = new ArrayList<>();
+        String ini = "";
+        int i = 0;
+        while (!ini.equals(target)) {
+            String to = target.substring(0, i + 1);
+            char ch = 'a';
+            StringBuilder sb = new StringBuilder(ini);
+
+            while (!sb.toString().equals(to)) {
+                if (sb.length() < to.length()) {
+                    sb.append('a');
+                } else {
+                    if (sb.charAt(sb.length() - 1) == 'z') {
+                        sb.setCharAt(sb.length() - 1, 'a');
+                    } else {
+                        sb.setCharAt(sb.length() - 1, (char) (sb.charAt(sb.length() - 1) + 1));
+                    }
                 }
-                else lastdigit++;
-                sb.setCharAt(sb.length()-1,lastdigit);
                 list.add(sb.toString());
             }
+
+            ini = sb.toString();
+            i++;
         }
+
         return list;
     }
 }
