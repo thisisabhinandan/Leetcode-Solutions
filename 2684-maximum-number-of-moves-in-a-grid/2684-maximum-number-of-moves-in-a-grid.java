43f197dbebcceef81530b56d46f68d0c99eb5d5e
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-10-29 13:35:23 +0530

    Time: 15 ms (50.00%), Space: 56.2 MB (60.15%) - LeetHub

--- 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
+++ 2684-maximum-number-of-moves-in-a-grid/2684-maximum-number-of-moves-in-a-grid.java
@@ -3,47 +3,35 @@ class Solution {
         int n = grid.length; 
         int m = grid[0].length; 
         int [][] dp = new int[n][m];
-        
-        // Initialize the dp array with -1 to indicate unreachable cells
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                dp[i][j] = -1;
-            }
-        }
-        
-        // Since we can start from any cell in the first column, set dp[i][0] = 0
-        for(int i = 0; i < n; i++) {
-            dp[i][0] = 0;
-        }
-        
-        // Iterate over the grid to fill dp values
-        for(int j = 0; j < m - 1; j++) {  // Columns from 0 to m - 2
-            for(int i = 0; i < n; i++) {
-                if(dp[i][j] != -1) {  // If the current cell is reachable
-                    int currentValue = grid[i][j];
-                    // Move to (i - 1, j + 1)
-                    if(i > 0 && grid[i - 1][j + 1] > currentValue) {
-                        dp[i - 1][j + 1] = Math.max(dp[i - 1][j + 1], dp[i][j] + 1);
-                    }
-                    // Move to (i, j + 1)
-                    if(grid[i][j + 1] > currentValue) {
-                        dp[i][j + 1] = Math.max(dp[i][j + 1], dp[i][j] + 1);
-                    }
-                    // Move to (i + 1, j + 1)
-                    if(i < n - 1 && grid[i + 1][j + 1] > currentValue) {
-                        dp[i + 1][j + 1] = Math.max(dp[i + 1][j + 1], dp[i][j] + 1);
-                    }
+        for(int i=0;i<n;i++)
+        {
+            dp[i][0]=1;
+        } int ans=0;
+        for(int j=1;j<m;j++)
+        {
+            for(int i=0;i<n;i++)
+            {
+                if(i+1<n && grid[i][j]>grid[i+1][j-1] && dp[i+1][j-1]!=0)
+                {
+                    dp[i][j]=Math.max(1+dp[i+1][j-1],dp[i][j]);
+                }
+                if(i-1>=0 && grid[i][j]>grid[i-1][j-1] && dp[i-1][j-1]!=0)
+                {
+                    dp[i][j]=Math.max(1+dp[i-1][j-1],dp[i][j]);
+                }
+                if(grid[i][j]>grid[i][j-1] && dp[i][j-1]!=0)
+                {
+                    dp[i][j]=Math.max(1+dp[i][j-1],dp[i][j]);
                 }
             }
         }
-        
-        // Find the maximum moves from the dp array
-        int ans = 0; 
-        for(int i = 0; i < n; i++) {
-            for(int j = 0; j < m; j++) {
-                ans = Math.max(ans, dp[i][j]);
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<m;j++)
+            {
+                ans=Math.max(ans,dp[i][j]);
             }
         }
-        return ans;
+        return ans-1;
     }
 }
