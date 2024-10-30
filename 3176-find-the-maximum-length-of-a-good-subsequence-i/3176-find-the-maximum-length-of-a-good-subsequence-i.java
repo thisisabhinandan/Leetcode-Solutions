f11e9c283d906201a83556f824883f41f8cd2105
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-10-30 19:03:06 +0530

    Time: 80 ms (58.82%), Space: 44.8 MB (49.71%) - LeetHub

---
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int maximumLength(int[] nums, int K) {
+        int n=nums.length; 
+        int [][] dp=new int [n][K+1];
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=K;j++)
+            {
+              dp[i][j]=1;    
+            }
+        } 
+         int ans = 1;
+        for (int k = 0; k <= K; k++) {
+            for (int i = 1; i < n; i++) {
+                for (int j = 0; j < i; j++) {
+                    if (nums[j] == nums[i]) {
+                        dp[i][k] = Math.max(dp[i][k], dp[j][k] + 1);
+                    } else if (k > 0) {
+                        dp[i][k] = Math.max(dp[i][k], dp[j][k - 1] + 1);
+                    }
+                }
+                ans = Math.max(ans, dp[i][k]);
+            }
+        }
+        
+        return ans;
+    }
+}
