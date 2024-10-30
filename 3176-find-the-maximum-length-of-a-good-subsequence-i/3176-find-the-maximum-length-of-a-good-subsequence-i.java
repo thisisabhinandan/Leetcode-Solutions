91abfd783af17423baf9897131e85c5bbf872110
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-10-30 19:13:19 +0530

    Time: 84 ms (58.82%), Space: 44.3 MB (66.47%) - LeetHub

--- 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.java
+++ 3176-find-the-maximum-length-of-a-good-subsequence-i/3176-find-the-maximum-length-of-a-good-subsequence-i.java
@@ -9,20 +9,28 @@ class Solution {
               dp[i][j]=1;    
             }
         } 
-         int ans = 1;
-        for (int k = 0; k <= K; k++) {
-            for (int i = 1; i < n; i++) {
-                for (int j = 0; j < i; j++) {
-                    if (nums[j] == nums[i]) {
-                        dp[i][k] = Math.max(dp[i][k], dp[j][k] + 1);
-                    } else if (k > 0) {
-                        dp[i][k] = Math.max(dp[i][k], dp[j][k - 1] + 1);
+        int ans=1;
+        for(int k=0;k<=K;k++)
+        {
+            for(int i=1;i<n;i++)
+            {
+                for(int j=0;j<i;j++)
+                {
+                    if(nums[j]!=nums[i])
+                    {
+                        if(k>0)
+                        {
+                            dp[i][k]=Math.max(dp[i][k],dp[j][k-1]+1);
+                        }
+                    }
+                    else
+                    {
+                        dp[i][k]=Math.max(dp[i][k],dp[j][k]+1);
                     }
                 }
-                ans = Math.max(ans, dp[i][k]);
+                ans=Math.max(ans,dp[i][k]);
             }
         }
-        
         return ans;
     }
 }
