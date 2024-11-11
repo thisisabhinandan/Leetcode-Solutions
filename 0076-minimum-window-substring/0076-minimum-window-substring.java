8cb90328a19b504936b8defc82a8ffbb3241431b
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-11-11 11:42:44 +0530

    Time: 119 ms (15.25%), Space: 45.5 MB (24.35%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.java
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -1,29 +1,52 @@
-  class Solution {
-                public String minWindow(String s, String t) {
-                    int tmap[]=new int[128]; int cnt=t.length(); int j=0; int minstart=0; int minlen=Integer.MAX_VALUE;
-                    for(int i=0;i<t.length();i++){
-                        char ch=t.charAt(i);
-                        tmap[ch]++;
-                    }
-                    for(int i=0;i<s.length();i++){
-                        char ch=s.charAt(i);
-                        if(tmap[ch]>0){
-                            cnt--;
-                        }
-                        tmap[ch]--;
-                        while(cnt==0)
-                        {
-                            if(minlen>i-j+1)
-                            {
-                                minstart=j;
-                                minlen=i-j+1;
-                            }
-                            tmap[s.charAt(j)]++;
-                            if(tmap[s.charAt(j)]>0) {cnt++;}
-                            j++;
-                        }
-                    }
-                    if(minlen==Integer.MAX_VALUE) return "";
-                    else return s.substring(minstart,minstart+minlen);
-                }
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public String minWindow(String s, String t) {
+        HashMap<Character, Integer> mp1 = new HashMap<>(); 
+        HashMap<Character, Integer> mp2 = new HashMap<>();
+        int j = 0, ans = Integer.MAX_VALUE;
+        String str = "";
+        
+        // Populate mp2 with character frequencies from t
+        for (int i = 0; i < t.length(); i++) {
+            char ch = t.charAt(i);
+            mp2.put(ch, mp2.getOrDefault(ch, 0) + 1); 
+        }
+        
+        int requiredMatches = mp2.size();
+        int formedMatches = 0;
+
+        // Start sliding window
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            mp1.put(ch, mp1.getOrDefault(ch, 0) + 1);
+            
+            // Check if current character matches required frequency
+            if (mp2.containsKey(ch) && mp1.get(ch).equals(mp2.get(ch))) {
+                formedMatches++;
             }
+            
+            // When we have all required characters, try to shrink the window
+            while (formedMatches == requiredMatches) {
+                if (i - j + 1 < ans) {
+                    ans = i - j + 1;
+                    str = s.substring(j, i + 1);
+                }
+                
+                // Shrink window from the left by reducing the frequency of s[j]
+                char leftChar = s.charAt(j);
+                mp1.put(leftChar, mp1.get(leftChar) - 1);
+                
+                // If a character in mp2 goes below required count, reduce match count
+                if (mp2.containsKey(leftChar) && mp1.get(leftChar) < mp2.get(leftChar)) {
+                    formedMatches--;
+                }
+                
+                j++; // Move the left pointer
+            }
+        }
+        
+        return str;
+    }
+}
