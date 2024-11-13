f80f6d19299caea43d15e57cd8143dbfee564431
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-11-13 18:21:15 +0530

    Time: 32 ms (5.08%), Space: 48.2 MB (74.49%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,16 +1,31 @@
+import java.util.TreeMap;
+
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        TreeMap<Integer,Integer> map = new TreeMap<>();
-        for(int i: g) map.put(i,map.getOrDefault(i,0)+1);
-        int counter = 0;
-        for(int i: s){
-            Integer child = map.floorKey(i);
-            if(child!=null){
-                counter++;
-                map.put(child,map.get(child)-1);
-                if(map.get(child)==0) map.remove(child);
+        TreeMap<Integer, Integer> mp = new TreeMap<>();
+        int cnt = 0;
+
+        // Populate the TreeMap with cookie sizes and their frequencies
+        for (int cookie : s) {
+            mp.put(cookie, mp.getOrDefault(cookie, 0) + 1);
+        }
+
+        // Try to satisfy each child
+        for (int greed : g) {
+            Integer child = mp.ceilingKey(greed); // Find the smallest cookie >= greed
+
+            if (child != null) { // If a suitable cookie is found
+                cnt++; // Increase count of satisfied children
+                int frequency = mp.get(child);
+                
+                if (frequency == 1) {
+                    mp.remove(child); // Remove the cookie if its frequency becomes zero
+                } else {
+                    mp.put(child, frequency - 1); // Decrement the frequency of the cookie
+                }
             }
         }
-        return counter;
+
+        return cnt;
     }
 }
