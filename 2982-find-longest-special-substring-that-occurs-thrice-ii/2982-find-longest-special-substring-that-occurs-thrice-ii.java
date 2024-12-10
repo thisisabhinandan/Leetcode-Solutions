7b1516ea2c90414803e6efc9ae76c6ff4ae6a86c
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-12-10 14:17:18 +0530

    Time: 91 ms (63.24%), Space: 45.8 MB (94.87%) - LeetHub

--- 2982-find-longest-special-substring-that-occurs-thrice-ii/2982-find-longest-special-substring-that-occurs-thrice-ii.java
+++ 2982-find-longest-special-substring-that-occurs-thrice-ii/2982-find-longest-special-substring-that-occurs-thrice-ii.java
@@ -1,44 +1,55 @@
+import java.util.*;
+
 class Solution {
     public int maximumLength(String s) {
-        Map<Character, List<Integer>> cnt = new HashMap<>();
         int n = s.length();
-
-        // 统计连续字符段的长度
-        int i = 0;
-        while (i < n) {
-            char currentChar = s.charAt(i);
-            int j = i;
-            while (j < n && s.charAt(j) == currentChar) {
-                j++;
-            }
-            cnt.computeIfAbsent(currentChar, x -> new ArrayList<>()).add(j - i);
-            i = j;
+        // Array to store the lengths of runs for each character (a-z)
+        List<Integer>[] charRuns = new ArrayList[26];
+        for (int i = 0; i < 26; i++) {
+            charRuns[i] = new ArrayList<>();
         }
 
-        int lo = 1, hi = n , res = -1;
+        // Preprocess: Find all runs of each character
+        for (int i = 0; i < s.length(); ) {
+            char ch = s.charAt(i);
+            int cnt = 1; // Start with the current character
+            i++;
+            while (i < s.length() && ch == s.charAt(i)) {
+                cnt++;
+                i++;
+            }
+            // Add the length of the run to the corresponding character's list
+            charRuns[ch - 'a'].add(cnt);
+        }
+
+        // Binary search on the length
+        int lo = 1, hi = n, res = -1;
         while (lo <= hi) {
             int mid = lo + (hi - lo) / 2;
-            if (check(cnt, mid)) {      
-                lo = mid + 1;      
+            if (check(charRuns, mid)) {
+                res = mid; // Update result if mid is feasible
+                lo = mid + 1; // Try for a longer length
             } else {
-                hi = mid - 1;
+                hi = mid - 1; // Try for a shorter length
             }
         }
 
-        return lo-1 == 0 ? -1 : lo-1;
+        return res == 0 ? -1 : res;
     }
-    private boolean check(Map<Character, List<Integer>> cnt, int length) {
-        for (List<Integer> vec : cnt.values()) {
+
+    // Check if there exists a character with at least 3 substrings of length `length`
+    private boolean check(List<Integer>[] charRuns, int length) {
+        for (List<Integer> runs : charRuns) {
             int count = 0;
-            for (int x : vec) {
-                if (x >= length) {
-                    count += x - length + 1;
+            for (int run : runs) {
+                if (run >= length) {
+                    count += (run - length + 1); // Number of substrings of length `length`
+                }
+                if (count >= 3) {
+                    return true; // Found 3 substrings of length `length`
                 }
             }
-            if (count >= 3) {
-                return true;
-            }
         }
-        return false;
+        return false; // No character has 3 substrings of length `length`
     }
 }
