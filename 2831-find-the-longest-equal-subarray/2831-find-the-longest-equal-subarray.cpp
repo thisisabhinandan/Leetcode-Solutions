d6ca305c1faa07a81a2a39afd17559caacb6632f
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-12-14 14:39:05 +0530

    Time: 304 ms (32.29%), Space: 187.4 MB (52.44%) - LeetHub

--- 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
+++ 2831-find-the-longest-equal-subarray/2831-find-the-longest-equal-subarray.cpp
@@ -1,25 +1,19 @@
 class Solution {
 public:
     int longestEqualSubarray(vector<int>& nums, int k) {
-        int j = 0; // Left end of the sliding window
-        unordered_map<int, int> mp; // Frequency map
-        int maxLength = 0; // Store the result
-        int freq = 0; // Maximum frequency in the current window
-
-        for (int i = 0; i < nums.size(); i++) {
-            mp[nums[i]]++; // Add the current element to the frequency map
-            freq = max(freq, mp[nums[i]]); // Update the max frequency in the window
-
-            // Check if the window is valid
-            while ((i - j + 1) - freq > k) {
-                mp[nums[j]]--; // Remove the leftmost element from the frequency map
-                j++; // Shrink the window
+        int j=0; map<int,int> mp; int ans=0; int freq=0;
+        for(int i=0;i<nums.size();i++)
+        {
+            mp[nums[i]]++;
+            freq=max(freq,mp[nums[i]]);
+            while(i-j+1-freq>k)
+            {
+                mp[nums[j]]--;
+                j++;
+                
             }
-
-            // Update the result
-            maxLength = max(maxLength, freq);
+            ans=max(ans,freq);
         }
-
-        return maxLength;
+        return ans;
     }
 };
