cf52d9d1962a5ab85e39e067434fd76c0fdfea9a
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-12-18 15:08:02 +0530

    Time: 9 ms (54.73%), Space: 59 MB (38.75%) - LeetHub

--- 0848-shifting-letters/0848-shifting-letters.java
+++ 0848-shifting-letters/0848-shifting-letters.java
@@ -1,20 +1,18 @@
 class Solution {
     public String shiftingLetters(String s, int[] shifts) {
-        int n = shifts.length;
-        StringBuilder ans = new StringBuilder();
-
-        // Compute suffix sum directly in shifts array
-        for (int i = n - 2; i >= 0; i--) {
-            shifts[i] = (shifts[i] + shifts[i + 1]) % 26;
+        int n=shifts.length; StringBuilder sb=new StringBuilder();
+        long [] suff=new long [n];
+        suff[n-1]=shifts[n-1]; 
+        for(int i=n-2;i>=0;i--)
+        {
+            suff[i]=suff[i+1]+shifts[i]; 
         }
-
-        // Apply shifts to characters
-        for (int i = 0; i < s.length(); i++) {
-            char ch = s.charAt(i);
-            int to = (ch - 'a' + shifts[i]) % 26; // Calculate final position in alphabet
-            ans.append((char) (to + 'a')); // Convert back to character
+        for(int i=0;i<s.length();i++)
+        {
+            long start=s.charAt(i)-'a';
+            long to=(start+suff[i])%26;
+            sb.append((char)(to+'a'));
         }
-
-        return ans.toString();
+        return sb.toString();
     }
 }
