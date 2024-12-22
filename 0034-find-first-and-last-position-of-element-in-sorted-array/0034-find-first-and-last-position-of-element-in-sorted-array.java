6d0c3a08e4d37baec4edeacb5edd6148577c2f07
Abhinandan <72436960+thisisabhinandan@users.noreply.github.com>
2024-12-22 20:01:37 +0530

    Time: 0 ms (100.00%), Space: 46.3 MB (9.65%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,49 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int [] ans={-1,-1}; 
-        
-        int low=0; int high=nums.length-1;
-        
-        while(low<=high)
+        int first=lowerbound(nums,target); 
+        int last=upperbound(nums,target); 
+        return new int[] {first,last}; 
+    }
+    public int lowerbound(int nums[], int target)
+    {
+        int ans=-1; 
+        int l=0; int r=nums.length-1; 
+        while(l<=r)
         {
-            int mid=low+(high-low)/2; 
-            if(nums[mid]==target)
+            int mid=l+(r-l)/2;
+            if(nums[mid]>=target)
             {
-                ans[1]=mid;
-                low = mid+1; 
+                if(nums[mid]==target)
+                {
+                     ans=mid;
+                }
+                r=mid-1;
             }
-            else if(nums[mid]<target)
-            {
-                low=mid+1;
+            else{
+                l=mid+1;
             }
-            else high=mid-1; 
-            
         }
-        
-        low=0;  high=nums.length-1;
-        
-        while(low<=high)
+        return ans; 
+    }
+     public int upperbound(int nums[], int target)
+    {
+        int ans=-1; 
+        int l=0; int r=nums.length-1; 
+        while(l<=r)
         {
-            int mid=low+(high-low)/2; 
-            if(nums[mid]==target)
+            int mid=l+(r-l)/2;
+            if(nums[mid]<=target)
             {
-                ans[0]=mid;
-                high = mid-1; 
+                if(nums[mid]==target)
+                {
+                     ans=mid;
+                }
+                l=mid+1;
             }
-            else if(nums[mid]<target)
-            {
-                low=mid+1;
+            else{
+                r=mid-1;
             }
-            else high=mid-1; 
-            
         }
-        
         return ans; 
     }
 }
